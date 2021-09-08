22a62bb453e4755eac9ee67f104a42307146eb73
Bershadsky Mihail Sergeevich <Michail.Bershadsky@ilimtimber.corp>
2021-09-08 15:31:53 +0800

    redone app to correct form of Router

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -2,7 +2,7 @@ import React from 'react';
 import {MainPage, CartPage} from '../pages';
 import AppHeader from '../app-header';
 import WithRestoService from '../hoc';
-import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+import {BrowserRouter as Route, Switch} from 'react-router-dom';
 
 import Background from './food-bg.jpg';
 
@@ -10,16 +10,16 @@ const App = ({RestoService}) => {
     RestoService.getMenuItems().then(res => console.log(res));
     return (
         <div style={{background: `url(${Background}) center center/cover no-repeat`}} className="app">
-            <Router>
+            
+                <AppHeader total={50}/>
                 <Switch>
-                    <Route path="/menu" exact><AppHeader total={50}/><MainPage/></Route>
-                    <Route path="/cart" exact><AppHeader total={50}/><CartPage/></Route>
-                    <Route path="/" exact>
-                        <AppHeader total={50}/>
+                    <Route path="/" exact><MainPage/></Route>
+                    <Route path="/cart" exact><CartPage/></Route>
+                    <Route exact>
                         <MainPage/>
                     </Route>
                 </Switch>
-            </Router>
+            
             
         </div>
     )
